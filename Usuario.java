7fb30551da6389672cd12b16f634079b9ec93ff8
MJGM <MJGM@example.com>
2014-12-12 12:41:00 +0100

    commit 2: creada clase Usuario se pasa por parametro el nombre del usuario al constructor

---
+++ Usuario.java
@@ -0,0 +1,22 @@
+
+/**
+ * Clase que simula un usuario.
+ * 
+ * @author Marcelino José Granda Muñoz
+ * @version (a version number or a date)
+ */
+public class Usuario
+{
+    // instance variables - replace the example below with your own
+    private String nombre;
+
+    /**
+     * Constructor for objects of class Usuario
+     */
+    public Usuario( String nombre)
+    {
+       this.nombre = nombre;
+    }
+
+   
+}
